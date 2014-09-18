2213b355cb59790892b018c6321d52b8635cd29d
Arindo Duque <aanduque@gmail.com>
2014-09-18 13:12:17 -0300

    Font Awesome Added

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '211a1472d862d5dacf6c2617656cde90');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '377d7a6c7ab930ee813564ec119aa03e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
